f1e822fbce90840ce5ac2cdd56cc0ab9bac034d2
Acostan13 <acostan@fau.edu>
2019-12-15 10:29:05 -0500

    Basic operators

--- JS Basics/script.js
+++ JS Basics/script.js
@@ -46,3 +46,36 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+
+
+/*****************************
+* Basic operators
+*/
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yeahMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older tha John');
+var x;
+console.log(typeof x);
