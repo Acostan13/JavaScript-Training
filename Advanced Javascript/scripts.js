38d819be20cf5abf7cd6454ca87229b9a2af71e8
Alex Costan <acostan.con@orangetheory.com>
2020-01-16 16:51:56 -0500

    passing functions as arguments

--- Advanced Javascript/scripts.js
+++ Advanced Javascript/scripts.js
@@ -85,3 +85,39 @@ function change(a, b) {
 change(age, obj)
 console.log(age) // 30
 console.log(obj.city) // San Francisco 
+
+// Passing functions as arguments
+var years = [1990, 1965, 1937, 2005, 1998]
+
+function arrayCalc(arr, fn) {
+    var arrRes = []
+    for(var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]))
+    }
+    return arrRes
+}
+
+function calculateAge(el) {
+    return 2020 - el
+}
+
+function isFullAge(el) {
+    return el >= 18
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el))
+    } else {
+        return -1
+    }
+}
+
+var ages = arrayCalc(years, calculateAge)
+console.log(ages) // [26, 51, 79, 11, 18]
+
+var fullAges = arrayCalc(ages, isFullAge)
+console.log(fullAges) // [true, true, true, false, true]
+
+var rates = arrayCalc(ages, maxHeartRate)
+console.log(rates) // [187, 170, -1, -1, 192]
