05da7cb886285177aeef24342cc6108cc1b75fb5
Alex Costan <acostan.con@orangetheory.com>
2020-01-17 11:18:24 -0500

    bind, call, and apply methods

--- Advanced Javascript/scripts.js
+++ Advanced Javascript/scripts.js
@@ -209,3 +209,86 @@ function interviewQuestion(job) {
 interviewQuestion('designer')('John')
 interviewQuestion('teacher')('James')
 interviewQuestion('other')('Jana')
+
+// Bind, Call, and Apply
+
+var john = {
+    name: 'John',
+    age: 28,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ' Ladies & Gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.')
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' +  timeOfDay)
+        }
+    }
+}
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+}
+
+john.presentation('formal', 'morning')
+
+// Method Borrowing
+// Use call method when you have 1 or more arguments to pass
+john.presentation.call(emily, 'friendly', 'afternoon')
+
+// Use apply method when you want to pass an array of arguments
+john.presentation.apply(emily, ['friendly', 'afternoon'])
+
+// Use bind method when you want to preset arguments, aka currying => create a function based on another function with preset parameters
+var johnFriendly = john.presentation.bind(john, 'friendly')
+johnFriendly('morning')
+johnFriendly('night')
+
+var emilyFormal = john.presentation.bind(emily, 'formal')
+emilyFormal('afternoon')
+emilyFormal('night')
+
+// Challenge
+var years = [1990, 1965, 1937, 2005, 1998]
+
+function arrayCalc(arr, fn) {
+    var arrRes = []
+    for(var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]))
+    }
+    return arrRes
+}
+
+function calculateAge(el) {
+    return 2020 - el
+}
+
+function isFullAge(limit, el) {
+    return el >= limit
+}
+
+var ages = arrayCalc(years, calculateAge)
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20))
+console.log(ages) // var years = [1990, 1965, 1937, 2005, 1998]
+
+function arrayCalc(arr, fn) {
+    var arrRes = []
+    for(var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]))
+    }
+    return arrRes
+}
+
+function calculateAge(el) {
+    return 2020 - el
+}
+
+function isFullAge(limit, el) {
+    return el >= limit
+}
+
+var ages = arrayCalc(years, calculateAge)
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20))
+console.log(ages) // [30, 55, 83, 15, 12]
+console.log(fullJapan) // [true, true, true, false, true]
