3c9a61c27f5de5f5e61e4470714ca37219dd9e34
Alex Costan <acostan.con@orangetheory.com>
2020-08-11 10:49:49 -0400

    rolling the dice event handling

--- DOM pig game/app.js
+++ DOM pig game/app.js
@@ -9,19 +9,35 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice
+var scores, roundScore, activePlayer
 
 scores = [0,0]
 roundScore = 0
 activePlayer = 1
 
-dice = Math.floor(Math.random() * 6) + 1
-
-document.querySelector('#current-' + activePlayer).textContent = dice
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-
-var x = document.querySelector('#score-0').textContent
-console.log(x) // 0
-
 document.querySelector('.dice').style.display = 'none'
 
+// Selecting elements by ID is slightly faster than querySelector
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.src = 'dice-' + dice + '.png'
+    diceDOM.style.display = 'block'
+    
+    // 3. Update the round score IF the rolled number was NOT a 1
+
+
+})
+
+// document.querySelector('#current-' + activePlayer).textContent = dice
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+//var x = document.querySelector('#score-0').textContent
+//console.log(x) // 0
