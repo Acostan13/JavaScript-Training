41be2bcd417a686e86ccea33eec9f323fbd6ed75
Alex Costan <acostan.con@orangetheory.com>
2020-08-13 11:47:59 -0400

    closures

--- Advanced Javascript/scripts.js
+++ Advanced Javascript/scripts.js
@@ -172,3 +172,40 @@ game()
     var score = Math.random() * 10
     console.log(score => 5 - goodLuck)
 })(5)
+
+// Closures
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.'
+    return function(yearOfBirth) {
+        var age = 2020 - yearOfBirth
+        console.log((retirementAge - age) + a)
+    }
+}
+
+var retirementUS = retirement(66)
+var retirementGermany = retirement(65)
+var retirementIceland = retirement(67)
+
+retirementUS(1990) // 36 years left until retirement.
+retirementGermany(1990) // 35 years left until retirement.
+retirementIceland(1990) // 37 years left until retirement.
+
+retirement(66)(1990) // 36 years left until retirement.
+
+// Challenge
+function interviewQuestion(job) {
+    return function(name) {
+      if(job === 'designer') {
+        console.log(name + ', can you please explain what UX design is?')
+      } else if (job == 'teacher') {
+        console.log('What subject do you teach, ' + name + '?')
+      } else {
+        console.log('Hello ' + name + ', what do you do?')
+      }
+    }
+}
+  
+interviewQuestion('designer')('John')
+interviewQuestion('teacher')('James')
+interviewQuestion('other')('Jana')
