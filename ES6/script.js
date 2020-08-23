f5610cdc895d4386a5456daf70e4da328574d42a
Alex Costan <acostan.con@orangetheory.com>
2020-08-23 11:40:45 -0400

    arrays in ES6

--- ES6/script.js
+++ ES6/script.js
@@ -207,3 +207,46 @@ function calcAgeRetirement(year) {
 // Destructuring an array
 const [age, retirement] = calcAgeRetirement(1990)
 console.log(age, retirement) // => 30, 35
+
+// Arrays in ES6
+
+const boxes = document.querySelectorAll('.box')
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes)
+boxesArr5.forEach(function(cur){
+  cur.style.backgroundColor = 'dodgerblue'
+})
+
+// ES6
+Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue')
+
+// ES5
+for(var i = 0; i < boxesArr5.length; i++) {
+  if (boxesArr5[i].className === 'boxblue') {
+    continue
+  }
+  boxesArr5[i].textContent = 'Changed to blue!'
+}
+
+// ES6
+for (const cur of boxesArr6) {
+  if (cur.className.includes('blue')) {
+    continue
+  }
+   cur.textContent = 'Changed to blue!'
+}
+
+// ES5
+var ages = [12, 17, 8, 21, 14, 11]
+
+var full = ages.map(function(cur) {
+  return cur >= 18
+})
+console.log(full) // [false, false, false, true, false, false]
+console.log(full.indexOf(true)) // 3
+console.log(ages[full.indexOf(true)])
+
+// ES6
+ages.findIndex(cur => cur >= 18) // 3
+ages.find(cur => cur >= 18) // 21
