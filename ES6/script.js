c73f4aa2c4ed7318e20d6e7ba4457e72494129e1
Alex Costan <acostan.con@orangetheory.com>
2020-08-23 21:21:23 -0400

    maps

--- ES6/script.js
+++ ES6/script.js
@@ -330,6 +330,38 @@ function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
   this.nationality = nationality
 }
 
-
 var john = new SmithPerson('John', 1990) 
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish')
+
+// Maps
+
+const question = new Map()
+
+question.set('question', 'What is the official name of the latest major Javascript version?')
+question.set(1, 'ES5')
+question.set(2, 'ES6')
+question.set(3, 'ES2015')
+question.set(4, 'ES7')
+question.set('correct', 3)
+question.set(true, 'Correct answer :D')
+question.set(false, 'Wrong, please try again')
+
+question.get('question') // What is the official name of the latest major Javascript version?
+question.get(question.size) // 8
+
+if (question.has(4)) {
+  question.delete(4) // removes key 4
+}
+
+question.clear() // clears entire object
+
+question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`))
+
+for (let [key, value] of question.entries()) {
+  if (typeof(key) === 'number') {
+    console.log(`Answer ${key} : ${value}`)
+  }
+}
+
+const ans = parseInt(prompt('Write the correct answer'))
+question.get(ans === question.get('correct'))
