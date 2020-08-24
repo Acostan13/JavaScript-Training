3ea9332d6c5bbdf013d7214fc909c227e396d770
Alex Costan <acostan.con@orangetheory.com>
2020-08-23 21:33:34 -0400

    classes

--- ES6/script.js
+++ ES6/script.js
@@ -365,3 +365,41 @@ for (let [key, value] of question.entries()) {
 
 const ans = parseInt(prompt('Write the correct answer'))
 question.get(ans === question.get('correct'))
+
+// Classes
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+}
+
+Person.prototype.calculateAge = function() {
+  var age = new Date().getFullYear - this.yearOfBirth
+  console.log(age)
+}
+
+var john6 = new Person5('John', 1990, 'teacher')
+
+// ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name
+    this.yearOfBirth = yearOfBirth
+    this.job = job
+  }
+
+  calculateAge() {
+    var age = new Date().getFullYear - this.yearOfBirth
+    console.log(age)
+  }
+
+  static greeting() {
+    console.log('Hey there!')
+  }
+}
+
+const john6 = new Person6('John', 1990, 'teacher')
+
+Person6.greeting() // Hey there!
