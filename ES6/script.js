2e107d7c3b5c4313f102639c39779780690b4f01
Alex Costan <acostan.con@orangetheory.com>
2020-08-24 21:34:31 -0400

    setup class, subclasses, and park/street data

--- ES6/script.js
+++ ES6/script.js
@@ -451,13 +451,56 @@ johnAthlete6.calculateAge() // 30
 // 1. Parks
 // 2. Streets
 
-
 // It's a very small town, as there are only 3 parks and 4 streets. All the parks and streets have a name and a build year.
+
 // At an end-of-year meeting, your boss wants a final reports with the following:
 // 1. Tree density of each park in the town (formula: number of trees/park area)
 // 2. The name of the park that has more than 1000 trees
-// 4. Total and average length of thw town's streets
+// 4. Total and average length of the town's streets
 // 5. Size classification of all streets: tiny/small/normal/big/huge
 // If the size is unknown, the default is normal
 
 // All the report data should be printed to the console
+
+class Element {
+  constructor (name, buildYear) {
+    this.name = name
+    this.buildYear = buildYear
+  }
+}
+
+class Park extends Element {
+  constructor (name, buildYear, area, numTrees) {
+    super(name, buildYear)
+    this.area = area
+    this.numTrees = numTrees
+  }
+
+  treeDensity() {
+    const density = this.numTrees / this.area
+    console.log(`${this.name} has a tree density of ${density} trees per square km.`) 
+  }
+
+}
+
+class Street extends Element {
+  constructor(name, buildYear, length, size = 3) {
+    super(name, buildYear)
+    this.length = length
+    this.size = size
+  }
+
+  classifyStreet() {
+    const classification = new Map()
+    classification.set(1, 'tiny')
+    classification.set(2, 'small')
+    classification.set(3, 'normal')
+    classification.set(4, 'big')
+    classification.set(5, 'huge')
+    console.log(`${this.name}, built in ${buildYear}, is a ${classification.get(this.size)}`)
+  }
+}
+
+const allParks = [new Park('Green Park', 1987, 0.2, 215), new Park('National Park', 1894, 2.9, 3541), new Park('Oak Park', 1953, 0.4, 949)]
+
+const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4), new Street('Evergreen', 2008, 2.7, 2), new Street('4th Street', 2015, 0.8), new Street('Sunset Boulevard', 1982, 2.5, 5)]
